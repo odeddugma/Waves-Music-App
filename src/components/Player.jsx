0bfc7011c3b42150efbb28c413752bfc85a00bc2
Oded Dugma <odeddugma@gmail.com>
2021-12-13 11:02:00 +0200

    Add useRef and playSongHandler

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faPlay,
@@ -5,7 +6,16 @@ import {
 	faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+	const { audio } = currentSong;
+	// Ref
+	const audioRef = useRef(null);
+
+	// Events Handlers
+	const playSongHandler = () => {
+		console.log(audioRef.current);
+	};
+
 	return (
 		<div className="player-container">
 			<div className="time-control">
@@ -15,13 +25,19 @@ const Player = () => {
 			</div>
 			<div className="play-control">
 				<FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-				<FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+				<FontAwesomeIcon
+					className="play"
+					size="2x"
+					icon={faPlay}
+					onClick={playSongHandler}
+				/>
 				<FontAwesomeIcon
 					className="skip-forward"
 					size="2x"
 					icon={faAngleRight}
 				/>
 			</div>
+			<audio src={audio} ref={audioRef} />
 		</div>
 	);
 };
